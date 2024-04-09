63e9a8254ebf2261e8bf0519a4960d7ae3bccee8
ccordero14 <camicorderogandica@gmail.com>
2024-04-09 03:26:10 -0300

    g y fog

--- test.hs
+++ test.hs
@@ -1,6 +1,17 @@
+import System.Win32 (xBUTTON1)
 doubleMe x = x + x
 
 f :: Int -> Int
-f 5 = 0
-f 4 = 8
-f 7 = 6
+f 1 = 8
+f 4 = 131
+f 16 = 16
+
+g :: Int -> Int
+g 8 = 16
+g 16 = 4
+g 131 = 1
+
+h :: Int -> Int
+h = f . g
+    
+
