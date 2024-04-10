a2bf3f6969a51cd70891619433cc39437630a10a
ccordero14 <camicorderogandica@gmail.com>
2024-04-09 22:53:29 -0300

    suma distintos

--- test.hs
+++ test.hs
@@ -21,4 +21,9 @@ maximo3 x y z | x > y && x > z = x
               | y > z && y > x = y
               | z > y && z > x = z
 
-
+sumaDistintos :: Int -> Int -> Int -> Int 
+sumaDistintos x y z | x /= y && x /= z && y /= z = x + y + z
+                    | x == y && x /= z && y /= z = x + z
+                    | x /= y && x == z && y /= z = x + y
+                    | x /= y && x /= z && y == z = x + y
+                    | x == y && x == z && y == z = 0
