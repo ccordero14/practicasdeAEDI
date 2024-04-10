f2e6a1f360bf4175bd8ba10a0cf153adcb0af5a2
ccordero14 <camicorderogandica@gmail.com>
2024-04-10 02:25:18 -0300

    bisiesto y posPrimerPar, ambos copiados lmao

--- test.hs
+++ test.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant ==" #-}
 doubleMe x = x + x
 
 f :: Int -> Int
@@ -12,16 +14,16 @@ g 131 = 1
 
 h :: Int -> Int
 h = f . g
-    
+
 k :: Int -> Int
 k = g . f
 
-maximo3 :: Int -> Int -> Int -> Int 
+maximo3 :: Int -> Int -> Int -> Int
 maximo3 x y z | x > y && x > z = x
               | y > z && y > x = y
               | z > y && z > x = z
 
-sumaDistintos :: Int -> Int -> Int -> Int 
+sumaDistintos :: Int -> Int -> Int -> Int
 sumaDistintos x y z | x /= y && x /= z && y /= z = x + y + z
                     | x == y && x /= z && y /= z = x + z
                     | x /= y && x == z && y /= z = x + y
@@ -39,3 +41,15 @@ todoMenor [x , y] [z , d] | x < z && y < d = True
                           | otherwise = False
 
 --WE'RE DONEEEEEEEEE FUCK YEAH
+-- no tf we weren't
+
+posPrimerPar :: (Int, Int, Int) -> Int
+posPrimerPar (x, y, z) | even x = 1
+                       | even y = 2
+                       | even z = 3
+                       | otherwise = 4
+
+bisiesto1 :: Int -> Bool
+bisiesto1 x | mod x 100 == 0 = mod x 400 == 0
+            | otherwise = mod x 4 == 0
+
