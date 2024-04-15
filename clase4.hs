fccf4c682c9c98e079461d7659fa3d414f0316d0
ccordero14 <165959715+ccordero14@users.noreply.github.com>
2024-04-15 19:28:29 -0300

    Add files via upload

--- clase4.hs
+++ clase4.hs
@@ -28,5 +28,18 @@ sumaDigitos :: Integer -> Integer
 sumaDigitos x | x < 10 = x
               | otherwise = mod x 10 + sumaDigitos (div x 10)
 
- 
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales x | x < 10 = True 
+                      | ultimoDigito x == ultimoDigito (sacarUnidades x) = True
+                      | otherwise = False
+
+ultimoDigito :: Integer -> Integer
+ultimoDigito x = mod (absoluto x) 10
+
+sacarUnidades :: Integer -> Integer
+sacarUnidades x  = div (absoluto x) 10
+
+absoluto :: Integer -> Integer
+absoluto x | x < 0 = -x
+           | otherwise = x
 
