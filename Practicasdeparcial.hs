688db213d57c2e898338be9e6ac0b3fc4fe982fa
ccordero14 <camicorderogandica@gmail.com>
2024-04-29 17:32:04 -0300

    sori

---
+++ Practicasdeparcial.hs
@@ -0,0 +1,11 @@
+module Practicasdeparcial where
+
+{--Ejercicio 1
+problema relacionesValidas (relaciones: seq⟨String x String⟩) : Bool {
+  requiere: {True}
+  asegura: {(res = true) <=> relaciones no contiene ni tuplas repetidas1, ni tuplas con ambas componentes iguales}
+}
+A los fines de este problema consideraremos que dos tuplas son iguales si el par de elementos que las componen (sin importar el orden) son iguales.}
+
+
+
