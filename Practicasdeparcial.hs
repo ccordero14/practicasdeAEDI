473cc80dd076f39d417c37e7881a562397eafb68
ccordero14 <camicorderogandica@gmail.com>
2024-04-30 16:52:37 -0300

    ITFUCKINGWORKEDYES

--- Practicasdeparcial.hs
+++ Practicasdeparcial.hs
@@ -28,27 +28,63 @@ relacionesValidas ((x,y):(a,b):ys) | iguales (x,y) ((a,b):ys) = False
   asegura: {res no tiene elementos repetidos}
   asegura: {res tiene exactamente los elementos que figuran en alguna tupla de relaciones, en cualquiera de sus posiciones}-}
 
-pertenece :: (Eq t) => t -> [t] -> Bool
-pertenece _ [] = False
-pertenece n (x:xs) | n == x = True
-                   | otherwise = pertenece n xs
+{--ejercicio2
+  problema personas (relaciones: seqâŸ¨String x StringâŸ©) : seqâŸ¨StringâŸ© {
+  requiere: {relacionesValidas(relaciones)}
+  asegura: {res no tiene elementos repetidos}
+  asegura: {res tiene exactamente los elementos que figuran en alguna tupla de relaciones, en cualquiera de sus posiciones}-}
 
-listaPersonas :: [(String, String)] -> [String]
-listaPersonas [] = []
-listaPersonas ((x,y):xs) = x : y : listaPersonas xs 
-
-sacarRepetidos :: (Eq t) => [t] -> [t]
-sacarRepetidos [] = []
-sacarRepetidos (x:xs) | pertenece x xs = pasoRecursivo
-                      | otherwise = x : pasoRecursivo
-                      where pasoRecursivo = sacarRepetidos xs
-
-personas :: [(String,String)] -> [String]
+personas :: [(String, String)] -> [String]
 personas [] = []
-personas xs = sacarRepetidos (listaPersonas xs)
-
-                        
+personas (x:xs) = quitarRepetidos (elementoTupla x ++ personas xs)
 
 
+elementoTupla :: (String, String) -> [String]
+elementoTupla (x,y) = [x,y]
 
+pertenece :: String -> [String] -> Bool
+pertenece _ [] = False
+pertenece n (x:xs) = n == x || pertenece n xs
+
+quitarRepetidos :: [String] -> [String]
+quitarRepetidos [] = []
+quitarRepetidos (x:xs) | not (pertenece x xs) = x : quitarRepetidos xs
+                       | otherwise = quitarRepetidos xs
+
+{-Ejercicio 3
+problema amigosDe (persona: String, relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
+  requiere: {relacionesValidas(relaciones)}
+  asegura: {res tiene exactamente los elementos que figuran en las tuplas de relaciones en las que una de sus componentes es persona}
+}
+-}
+
+amigosDe :: String -> [(String, String)] -> [String]
+amigosDe n [] = []
+amigosDe n ((x,y):xs) | n == y = x : amigosDe n xs
+                      | n == x = y : amigosDe n xs
+                      | otherwise = []
+
+{--Ejercicio 4
+problema personaConMasAmigos (relaciones: seq⟨String x String⟩) : String {
+  requiere: {relaciones no vacía}
+  requiere: {relacionesValidas(relaciones)}
+  asegura: {res es el Strings que aparece más veces en las tuplas de relaciones (o alguno de ellos si hay empate)}
+}-}
+
+aparece :: String -> [(String, String)] -> Integer
+aparece x ((a,b):xs) | x == a = 1 + aparece x xs
+                     | x == b = 1 + aparece x xs 
+                     | otherwise = 0
+
+contadorApariciones :: [String] -> [(String, String)] -> [(String, Integer)]
+contadorApariciones [] _ = []
+contadorApariciones (x:xs) (y:ys) = (x, aparece x (y:ys)) : contadorApariciones xs (y:ys)
+
+quienApareceMasVeces :: [(String, Integer)] -> String
+quienApareceMasVeces [(a,x)] = a
+quienApareceMasVeces ((a,x):(b,y):xs) | x >= y = quienApareceMasVeces ((a,x):xs)
+                                      | otherwise = quienApareceMasVeces ((b,y):xs)
+
+personaConMasAmigos :: [(String, String)] -> String
+personaConMasAmigos (x:xs) = quienApareceMasVeces (contadorApariciones (personas (x:xs)) (x:xs))
 
