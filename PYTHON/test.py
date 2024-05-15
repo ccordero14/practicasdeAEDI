d866bc25b8a4410fab4debf9c044639fad0bcb91
Edu <bav.eduardo@gmail.com>
2024-05-15 19:25:57 -0300

    cosas

--- PYTHON/test.py
+++ PYTHON/test.py
@@ -2,7 +2,7 @@ def imprimir_hola_mundo ():
     print('¡Hola mundo!')
 
 def perimetro() -> float:
-    return  2*3.14
+    return  2*math.pi
 
 def es_multiplo_de(n:int, m:int)->bool:
     res: bool=((n/m)%1)==0 #mod n/m es valido tmb
@@ -19,4 +19,15 @@ def foo(t:int):
 
 def imprimir_verso_cancion ():
     print("im super shy, super shy\nbut wait a minute while i make you mine, make you mine\nkoreanlmao youre on my mind, all the time\ni wanna tell you but im super shy, super shy")
+
+import math
+
+def raizDe2()->float:
+    return round (math.sqrt (2) , 2)
+
+def factorial_2()->int:
+    return math.factorial (2)
+
+def imprimir_saludo(nombre:str)->str:
+    return "hola nombre"
 
