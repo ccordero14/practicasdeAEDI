a3d8376767d4d1b715134c10537570f7c5f7e6ee
Edu <bav.eduardo@gmail.com>
2024-05-20 19:25:06 -0300

    avanzamos

--- PYTHON/test.py
+++ PYTHON/test.py
@@ -1,3 +1,5 @@
+# ejercicio1:
+
 def imprimir_hola_mundo ():
     print('¡Hola mundo!')
 
@@ -28,6 +30,112 @@ def raizDe2()->float:
 def factorial_2()->int:
     return math.factorial (2)
 
+# ejercicio 2:
+
 def imprimir_saludo(nombre:str)->str:
-    return "hola nombre"
+    return "hola " + nombre
+
+def raiz_cuadrada_de(numero: float)->float:
+    return math.sqrt(numero)  
+
+def fahrenheit_a_celcius(tempF:float)->float:
+    return ((tempF - 32) * 5)/9
+
+def imprimir_dos_veces(estribillo:str)-> str:
+    return estribillo *2
+
+def es_multiplo_de(a:int, b:int)->bool:
+    res: bool= a%b==0
+    return res
+
+def es_par(a:int)-> bool:
+    res: bool= a%2==0
+    return res
+
+def cantidad_de_pizzas(comensales:int, min_cant_de_porciones: int)->int:
+    return math.ceil ((comensales*min_cant_de_porciones)/8)
+
+# ejercicio 3:
+
+def alguno_es_cero(a:int, b:int)-> bool:
+    res: bool= a==0 or b ==0
+    return res
+
+def ambos_son_cero(a:int, b:int)-> bool:
+    res: bool= a==0 and b ==0
+    return res
+
+def es_bisiesto(año:int)->bool:
+    return año%400 == 0 or año%4==0 and not año%100==0
+
+# ejercicio 4:
+
+def peso_pino(altura:int)->int:
+    if altura<=3 :
+        res= altura * 300
+    else:
+        res= 900 + ((altura - 3)*200)
+    return res
+
+def es_peso_util(peso:int)-> bool:
+    return peso>=400 and peso<=1000
+
+def sirve_pino(altura:int)->bool:
+    return es_peso_util(peso_pino(altura))
+
+# ejercicio 5:
+
+def devolver_el_doble_si_es_par(numero:int)->int:
+    if numero%2==0:
+        res= numero*2
+    else: 
+        res= numero
+
+    return res
+
+def devolver_valor_si_es_par_sino_el_que_sigue(numero:int)->int:
+        if numero%2==0:
+            res= numero
+        else:
+            res=numero+1
+        return res
+
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(numero:int)->int:
+    if numero%9==0:
+        res=numero*3
+    else:
+        if numero%3==0:
+            res=numero*2
+    return res
+
+def lindo_nombre(nombre:str)->str:
+    largo:int=len(nombre)
+    if largo>=5:
+        res= "Tu nombre tiene muchas letras!"
+    else: 
+        res= "Tu nombre tiene menos de 5 caracteres!"
+
+    return res
+
+def elRango(numero:int)->str:
+    if numero<5:
+        res="Menor a 5"
+    else:
+        if numero>=10 and numero<=20:
+            res="Entre 10 y 20"
+        else:
+            if numero>20:
+                res="Mayor a 20"
+            else:
+                res= " "
+            
+    return res
+
+def vacacionesOTrabajo(sexo:chr, edad:int)->str:
+    if sexo=="M" and edad>=65 or sexo=="F" and edad>=60 or sexo=="F" and edad<18 or sexo=="M" and edad<18:
+        res="Andá de vacaciones"
+    else:
+        if sexo=="M" and edad<65 or sexo=="F" and edad<60:
+            res="Te toca trabajar"
+    return res
 
